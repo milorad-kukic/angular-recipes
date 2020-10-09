51c2782a3ec02fdbcba63021fdfb31fb708a3518
Milorad Kukic <kukic.milorad@gmail.com>
2020-10-09 20:20:10 +0000

    Added guard to prevent visiting recipes url if not authenticated.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth-guard.service';
 import { LoginComponent } from './login/login.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
@@ -10,12 +11,15 @@ import { SignupComponent } from './signup/signup.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'recipes', component: RecipesComponent, children: [
-    { path: '', component: RecipeStartComponent },
-    { path: 'new', component: RecipeEditComponent },
-    { path: ':id', component: RecipeDetailComponent },
-    { path: ':id/edit', component: RecipeEditComponent }
-  ] },
+  {path: 'recipes', 
+    component: RecipesComponent, 
+    canActivate: [AuthGuard],
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: 'new', component: RecipeEditComponent },
+      { path: ':id', component: RecipeDetailComponent },
+      { path: ':id/edit', component: RecipeEditComponent }
+    ] },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent }
